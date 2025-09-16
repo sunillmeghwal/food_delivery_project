ae048ca683ee02a0ca2cbb8ca7c8b13b6afaddd8
Sunil Meghwal <sunillmeghwal@gmail.com>
2025-09-16 22:11:08 +0530

    api link using render

--- food-order/src/components/Checkout.jsx
+++ food-order/src/components/Checkout.jsx
@@ -1,18 +1,18 @@
-import { useContext, useActionState } from 'react';
+import { useContext, useActionState } from "react";
 
-import Modal from './UI/Modal.jsx';
-import CartContext from '../store/CartContext.jsx';
-import { currencyFormatter } from '../util/formatting.js';
-import Input from './UI/Input.jsx';
-import Button from './UI/Button.jsx';
-import UserProgressContext from '../store/UserProgressContext.jsx';
-import useHttp from '../hooks/useHttp.js';
-import Error from './Error.jsx';
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext.jsx";
+import { currencyFormatter } from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {
-  method: 'POST',
+  method: "POST",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 };
 
@@ -21,7 +21,7 @@ export default function Checkout() {
   const userProgressCtx = useContext(UserProgressContext);
 
   const { data, error, sendRequest, clearData } = useHttp(
-    'http://localhost:3000/orders',
+    "https://food-delivery-project-6w5o.onrender.com/orders",
     requestConfig
   );
 
@@ -53,11 +53,14 @@ export default function Checkout() {
     );
   }
 
-  const [formState, formAction, isSending] = useActionState(checkoutAction,null);
+  const [formState, formAction, isSending] = useActionState(
+    checkoutAction,
+    null
+  );
 
   let actions = (
     <>
-      <Button type='button' textOnly onClick={handleClose}>
+      <Button type="button" textOnly onClick={handleClose}>
         Close
       </Button>
       <Button>Submit Order</Button>
@@ -71,7 +74,7 @@ export default function Checkout() {
   if (data && !error) {
     return (
       <Modal
-        open={userProgressCtx.progress === 'checkout'}
+        open={userProgressCtx.progress === "checkout"}
         onClose={handleFinish}
       >
         <h2>Success!</h2>
@@ -80,7 +83,7 @@ export default function Checkout() {
           We will get back to you with more details via email within the next
           few minutes.
         </p>
-        <p className='modal-actions'>
+        <p className="modal-actions">
           <Button onClick={handleFinish}>Okay</Button>
         </p>
       </Modal>
@@ -88,22 +91,22 @@ export default function Checkout() {
   }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
       <form action={formAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label='Full Name' type='text' id='name' />
-        <Input label='E-Mail Address' type='email' id='email' />
-        <Input label='Street' type='text' id='street' />
-        <div className='control-row'>
-          <Input label='Postal Code' type='text' id='postal-code' />
-          <Input label='City' type='text' id='city' />
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
         </div>
 
-        {error && <Error title='Failed to submit order' message={error} />}
+        {error && <Error title="Failed to submit order" message={error} />}
 
-        <p className='modal-actions'>{actions}</p>
+        <p className="modal-actions">{actions}</p>
       </form>
     </Modal>
   );
